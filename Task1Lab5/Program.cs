79be4252841bebde6d95391792c93a69b443c959
Gusev_A_D <Gusev_A_D@AU-DISP3-ARM2>
2021-10-12 12:51:09 +0300

    Добавьте файлы проекта.

---
+++ Task1Lab5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 7;
+            float[] array = new float[n];
+            Console.WriteLine("Введите 7 чисел");
+            float S = 0;            
+            for (int i = 0; i < n; i++)
+            {            
+                array[i] = (float)Convert.ToDouble(Console.ReadLine()); //долго мучался пытаясь понять, как правильно записать данную строчку) Забыл про явное/неявное преобразование.
+                S += array[i];
+            }
+            Console.WriteLine("Среднее арифметическое чисел равно {0:f2}",S / n);
+            Console.ReadKey();
+        }
+    }
+}
